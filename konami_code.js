a30ce2766a776dcce381a1bcd9bec1fc358503c7
gcampbell391 <gcampbell391@gmail.com>
2020-03-30 19:11:19 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,27 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
 ];
 
 function init() {
-  // your code here
+    let index = 0
+    document.body.addEventListener("keydown", (event) => {
+        if (event.key === codes[index]) {
+            index++
+            if (index === codes.length) {
+                alert("Cheat Activated..")
+                index = 0
+            }
+        } else {
+            index = 0
+        }
+    })
 }
